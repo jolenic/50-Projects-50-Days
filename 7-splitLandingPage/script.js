16c1df003e3bb3dd25a7765e30c1c8cd1d2d4f71
Jennifer Olenic <jennifer.olenic@gmail.com>
2021-07-09 19:25:17 -0400

    re-upload

--- 7-splitLandingPage/script.js
+++ 7-splitLandingPage/script.js
@@ -7,3 +7,4 @@ left.addEventListener('mouseleave', () => container.classList.remove('hover-left
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+
